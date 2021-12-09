08760e55fc4ee756585311ef83e537f3fefea94f
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-09 22:40:35 +0900

    impl zsh.rs

---
+++ src/shell/zsh.rs
@@ -0,0 +1,34 @@
+use crate::shell::Shell;
+use std::path::Path;
+
+#[derive(Debug)]
+pub struct Zsh;
+
+impl Shell for Zsh {
+    fn path(&self, path: &Path) -> String {
+        format!("export PATH={:?}:$PATH", path.to_str().unwrap())
+    }
+
+    fn set_env_var(&self, name: &str, value: &str) -> String {
+        format!("export {}={:?}", name, value)
+    }
+
+    fn use_on_cd(&self, _config: &crate::config::MamimiConfig) -> String {
+        indoc::indoc!(
+            r#"
+                autoload -U add-zsh-hook
+                _mamimi_autoload_hook () {
+                    mamimi --log-level quiet local
+                }
+
+                add-zsh-hook chpwd _mamimi_autoload_hook \
+                    && _mamimi_autoload_hook
+            "#
+        )
+        .into()
+    }
+
+    fn as_clap_shell(&self) -> clap::Shell {
+        clap::Shell::Zsh
+    }
+}
