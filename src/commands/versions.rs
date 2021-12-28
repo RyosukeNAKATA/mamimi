c17abf815a4d037744483f76c2ed4cb593120592
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-28 17:42:58 +0900

    impl commands/versions

--- src/commands/versions.rs
+++ src/commands/versions.rs
@@ -1,3 +1,56 @@
 use crate::config::MamimiConfig;
 use crate::outln;
 use crate::python_version::{current_python_version, PythonVersion};
+use colored::Colorize;
+use log::debug;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MamimiError {
+    #[error(transparent)]
+    HttpError(#[from] reqwest::Error),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error(transparent)]
+    SemverError(#[from] semver::Error),
+}
+
+pub struct Versions {}
+
+impl crate::command::Cmmand for Versions {
+    type Error = MamimiError;
+
+    fn apply(&self, config: &MamimiConfig) -> Result<(), Self::Error> {
+        for entry in config.versions().read_dir().map_err(MamimiError::IoError)? {
+            let entry = entry.map_err(MamimiError::IoError)?;
+            if crate::python_version::is_dotfile(&entry) {
+                continue;
+            }
+
+            let path = entry.path();
+            let filename = path
+                .filename()
+                .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::NotFound))
+                .map_err(MamimiError::IoError)?
+                .to_str()
+                .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::NotFound))
+                .map_err(MamimiError::IoError)?;
+            let version = PythonVersion::parse(filename).map_err(MamimiError::SemverError)?;
+            let current_python_version = current_python_version(&config).ok().flatten();
+            debug!(
+                "Current Python Version: {}",
+                current_python_version.clone().unwrap()
+            );
+            if let Some(current_python_version) = current_python_version {
+                if current_python_version == version {
+                    outln!(config#info, "{} {}", "*".green(), version.to_string().green());
+                } else {
+                    outln!(config#info, "{} {}", " ", version);
+                }
+            } else {
+                outln!(config#info, "{} {}", " ", version);
+            };
+        }
+        Ok(())
+    }
+}
