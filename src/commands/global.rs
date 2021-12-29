595b07261e938811c18ce2ebcc3eb5d2a006fa60
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-29 17:51:22 +0900

    impl global

---
+++ src/commands/global.rs
@@ -0,0 +1,43 @@
+use crate::alias::create_alias;
+use crate::commands::versions;
+use crate::input_version::InputVersion;
+use crate::python_version::PythonVersion;
+use log::debug;
+use reqwest::Version;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MamimiError {
+    #[error(transparent)]
+    HttpError(#[from] reqwest::Error),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error("Requested version {version} is not currently installed")]
+    VersionNotFound { version: InputVersion },
+}
+
+pub struct Global {
+    pub version: InputVersion,
+}
+
+impl crate::command::Command for Global {
+    type Error = MamimiError;
+    fn apply(&self, config: &crate::config::MamimiConfig) -> Result<(), Self::Error> {
+        debug!("Use {} as the default version", &self.version);
+        let version = match self.version.clone() {
+            InputVersion::Full(PythonVersion::Semver(v)) => PythonVersion::Semver(v),
+            version => return Err(MamimiError::VersionNotFound { version }),
+        };
+        if !&config
+            .python_version_dir()
+            .join(self.version.to_string())
+            .exists()
+        {
+            return Err(MamimiError::VersionNotFound {
+                version: self.version.clone(),
+            });
+        }
+        create_alias(&config, "default", &version).map_err(MamimiError::IoError)?;
+        Ok(())
+    }
+}
---
+++ src/commands/mod.rs
@@ -0,0 +1,8 @@
+pub mod completions;
+pub mod global;
+pub mod init;
+pub mod install;
+pub mod install_list;
+pub mod local;
+pub mod uninstall;
+pub mod versions;
