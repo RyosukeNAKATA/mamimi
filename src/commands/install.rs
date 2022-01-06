05dca5860a0f3e187dc7598ab012dcdef830944f
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-01-06 17:54:41 +0900

    impl install.rs

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -2,18 +2,21 @@ use crate::alias::create_alias;
 use crate::archive::{self, extract::Error as ExtractError, extract::Extract};
 use crate::config::MamimiConfig;
 use crate::input_version::InputVersion;
-use crate::outln;
 use crate::python_version::{current_python_version, PythonVersion};
 use crate::version_file::get_user_version_for_directory;
+use crate::{command, outln};
 use anyhow::Result;
 use colored::Colorize;
 use dirs::config_dir;
 use log::debug;
+use num_cpus;
 use reqwest::Url;
+use std::env::current_dir;
 use std::error;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use tempfile;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -79,6 +82,47 @@ impl crate::command::Command for Install {
         };
         let installations_dir = config.python_versions_dir();
         let installation_dir = PathBuf::from(&installations_dir).join(version.to_string());
+
+        if installation_dir.exists() {
+            return Err(MamimiError::VersionAlreadyInstalled {
+                path: installation_dir,
+            });
+        }
+
+        let url = package_url(&version);
+        outln!(config #Info, "{} Downloading {}","==>".green(),format!("{}",url).green());
+        let response = reqwest::blocking::get(url)?;
+        if response.status() == 404 {
+            return Err(MamimiError::VersionNotFound {
+                version: current_version,
+            });
+        }
+
+        outln!(config #Info, "{} Extracting {}","==>".green(),format!("{}", url).green());
+        let tmp_installations_dir = installations_dir.join(".downloads");
+        std::fs::create_dir_all(&tmp_installations_dir).map_err(MamimiError::IoError)?;
+        let tmp_dir = tempfile::TempDir::new_in(&tmp_installations_dir)
+            .expect("Cannot generate a temp directory");
+        extract_archive_into(&tmp_dir, response)?;
+
+        outln!(config #Info,"{} Building {}","==>".green(),format!("Python {}",current_version).green());
+        let installed_directory = std::fs::read_dir(&tmp_dir)
+            .map_err(MamimiError::IoError)?
+            .next()
+            .ok_or(MamimiError::TarIsEmpty)?
+            .map_err(MamimiError::IoError)?;
+        let installed_directory = installed_directory.path();
+        build_package(
+            &installed_directory,
+            &installation_dir,
+            &self.configure_opts,
+        )?;
+
+        if !config.default_python_version_dir().exists() {
+            debug!("Use {} as the default Python version", current_version);
+            create_alias(&config, "default", &version).map_err(MamimiError::IoError)?;
+        }
+        Ok(())
     }
 }
 
@@ -93,5 +137,91 @@ fn extract_archive_into<P: AsRef<Path>>(
     extractor
         .extract_into(path)
         .map_err(|source| MamimiError::ExtractError { source })?;
+    Ok(())
+}
+
+fn package_url(version: &PythonVersion) -> Url {
+    debug!("package url");
+    Url::parse(&format!(
+        "https://www.python.org/ftp/python/{}/Python-{}.tar.xz",
+        version, version
+    ))
+    .unwrap()
+}
+
+#[cfg(unix)]
+fn archive(version: &PythonVersion) -> String {
+    format!("python-{}.tar.xz", version)
+}
+
+#[cfg(windows)]
+fn archive(version: &PythonVersion) -> String {
+    format!("python-{}.zip", version)
+}
+
+#[allow(clippy::unnecessary_wraps)]
+fn openssl_dir() -> Result<String, MamimiError> {
+    #[cfg(target_os = "macos")]
+    return Ok(String::from_utf8_lossy(
+        &Command::new("brew")
+            .arg("--prefix")
+            .arg("openssl@1.1")
+            .output()
+            .map_err(MamimiError::IoError)?
+            .stdout,
+    )
+    .trim()
+    .to_string());
+    #[cfg(not(target_os = "macos"))]
+    return Ok("/url/local".to_string());
+}
+
+fn build_package(
+    current_dir: &Path,
+    installed_dir: &Path,
+    configure_opts: &[String],
+) -> Result<(), MamimiError> {
+    debug!("./configure {}", configure_opts.join(" "));
+    let mut command = Command::new("sh");
+    command
+        .arg("configure")
+        .arg(format!("--prefix={}", installed_dir.to_str().unwrap()))
+        .args(configure_opts);
+
+    // Provide a default value for --with-openssl-dir
+    if !configure_opts
+        .iter()
+        .any(|opt| opt.starts_with("--with-openssl-dir"))
+    {
+        command.arg(format!("--with-openssl-dir={}", openssl_dir()?));
+    }
+
+    let configure = command
+        .current_dir(&current_dir)
+        .output()
+        .map_err(MamimiError::IoError)?;
+    if !configure.status.success() {
+        return Err(MamimiError::CannotBuildPython {
+            stderr: format!(
+                "configure failed: {}",
+                String::from_utf8_lossy(&configure.stderr).to_string()
+            ),
+        });
+    };
+    debug!("make -j {}", num_cpus::get().to_string());
+    let make = Command::new("make")
+        .arg("-j")
+        .arg(num_cpus::get().to_string())
+        .current_dir(&current_dir)
+        .output()
+        .map_err(MamimiError::IoError)?;
+    if !make.status.success() {
+        return Err(MamimiError::CannotBuildPython {
+            stderr: format!(
+                "make failed: {}",
+                String::from_utf8_lossy(&make.stderr).to_string()
+            ),
+        });
+    };
     Ok(())
 }
