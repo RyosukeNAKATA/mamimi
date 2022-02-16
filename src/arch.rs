af5317f3063c9a59bf1cdad00803b7a29e30e6ae
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-02-16 17:48:32 +0900

    add system_info.rs

--- src/arch.rs
+++ src/arch.rs
@@ -1,1 +1,18 @@
 use crate::python_version::PythonVersion;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Arch {
+    X86,
+    X64,
+    Arm64,
+    Armv7l,
+    Ppc64le,
+    Ppc64,
+    s390x,
+}
+
+#[cfg(unix)]
+/// handle common case: Apple Silicon / Python >= 3.8.9
+pub fn get_safe_arch<'a>(arch:&'a Arch,version:&PythonVersion)->&'a Arch{
+    use crate::system_info::
+}
