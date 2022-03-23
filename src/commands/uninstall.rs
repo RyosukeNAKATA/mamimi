347583f657f0f8a4df2d91df86e05d6113251f44
RyosukeNAKATA <ryosuke.nakata.2019@ds.musashino-u.ac.jp>
2022-03-23 19:54:05 +0900

    fix

--- src/commands/uninstall.rs
+++ src/commands/uninstall.rs
@@ -1,7 +1,32 @@
+use crate::config::MamimiConfig;
 use crate::input_version::InputVersion;
+use crate::outln;
+use crate::python_version::PythonVersion;
+use crate::symlink::remove_symlink_dir;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MamimiError {
+    #[error(transparent)]
+    HttpError(#[from] reqwest::Error),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error("Can't find the number of cores.")]
+    FromUtf8Error(#[from] std::string::FromUtf8Error),
+    #[error("Can't find version: {version}")]
+    VersionNotFound { version: InputVersion },
+    #[error("The reqwested version is not installable: {version}")]
+    NotInstallableVersion { version: PythonVersion },
+    #[error("We can't find the necessary envitonment to replace the Python version.")]
+    MamimipathNotFound,
+}
 
 pub struct Uninstall {
     version: Option<InputVersion>,
 }
 
-impl Uninstall {}
+impl super::command::Command for Uninstall {
+    type Error = MamimiError;
+
+    fn apply(&self, &config: MamimiConfig) -> Result<(), Self::Error> {}
+}
